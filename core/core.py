d71a22e3db26b9dfa7ca5db90346487c87990ed0
Andrew M Bates <andrewbates09@gmail.com>
2014-12-02 11:51:52 -0800

    initializing the core

---
+++ core/core.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+'''
+# Author:  Andrew M Bates (andrewbates09)
+# File:    core.py
+# Project: FERGUS
+# Notes:
+'''
+
+''' IMPORTS '''
+
+import os
+
+
+''' VARIABLES '''
+
+
+''' FUNCTIONS '''
+
+def establishDependencies():
+    '''
+    Double-check to make sure dependent files, dbs, and etc exist and
+    are up to date.
+    '''
+    return
+
+def verifyCommand():
+    '''
+    1. Verify if a command exists and is callable.
+        a. check core
+        b. check library
+        c. check sqldb
+    2. Call command appropriately.
+    '''
+    return
+
+def butleMonitor():
+    '''
+    Buffer and analyze incoming input for commands and parameters.
+    '''
+    return
+
+def startFergus():
+    '''
+    1. Check all files and requirements before running.
+    2. Check system and return information.
+    3. Begin main program loop to listen for commands and handle them.
+    '''
+    return
+
+startFergus()
